b841c71dc1cbd267f0cd5b7dbc59f029f2888772
lavanyaboddu31@gmail.com <lavanyaboddu31@gmail.com>
2023-07-11 10:22:43 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conAppone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
